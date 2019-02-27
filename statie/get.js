b3913a072fea1bb1467a11082a91b0b92ec75345
Taras Mankovski <tarasm@gmail.com>
2019-02-27 10:19:54 -0500

    Get cwd from process

--- statie/get.js
+++ statie/get.js
@@ -9,7 +9,7 @@ const { join } = require("path");
 const assert = require("assert");
 const fixturify = require("fixturify");
 
-const base = os.cwd();
+const base = process.cwd();
 
 // path where the directory containing value will be located
 const { STATIE_SOURCE } = env.process;
